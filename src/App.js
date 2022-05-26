04917cfb39cf71837119563d3c6352040fb499ec
Gerardo Guerrero <gerardoguerrero29@hotmail.com>
2022-05-25 21:33:24 -0300

    Clase 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Saludar from './components/saludar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.  prueba edicion  en reactjs.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <Saludar quien={"picho"} msj={"hola, como estas mi"}/>
+   </div>
   );
 }
 
---
+++ src/components/saludar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Saludar({msj,quien}) {
+    return <div> {msj + " " + quien}</div>;
+}
