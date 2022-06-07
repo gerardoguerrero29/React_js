2cd149bf5ea2c326bd9f57a44734faf6ed798a33
Gerardo Guerrero <gerardoguerrero29@hotmail.com>
2022-06-06 21:34:07 -0300

    entregable 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import ItemCount from "./components/ItemCount";
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 
 function App() {
+  
+  const onAdd=()=>{
+      alert('se agrego al carrito')
+    }
+  
   return <div>
+  
     <NavBar />
     <ItemListContainer greeting={"Hola, soy un producto del carrito"} />
-
+    <ItemCount stock={10} initial={1} onAdd />
   </div>;
 
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export default function ItemCount({ stock, initial, onAdd }) {
+
+    const [i, setI] = useState(initial);
+    const [stk, setStk] = useState(stock);
+
+    return (<>
+        <button onClick={() => { i == 0 ? setI(0) : setI(i - 1) }}>-</button>
+        {i}
+        <button onClick={() => { setI(i + 1) }}>+</button>
+
+        <br />
+
+        <button onClick={() => { i <= stk ? onAdd() : alert('ERROR: la cantidad supera al stock disponible') }}>Agregar al Carrito</button>
+
+    </>
+    )
+}
