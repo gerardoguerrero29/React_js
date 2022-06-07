21bdc29a068cb611deb25829d279f81fc47b5034
Gerardo Guerrero <gerardoguerrero29@hotmail.com>
2022-06-06 21:41:43 -0300

    entregable 4

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
 
 function App() {
-  
-  const onAdd=()=>{
-      alert('se agrego al carrito')
-    }
-  
+
+  const onAdd = () => {
+    alert('se agrego al carrito')
+  }
   return <div>
-  
     <NavBar />
     <ItemListContainer greeting={"Hola, soy un producto del carrito"} />
     <ItemCount stock={10} initial={1} onAdd />
