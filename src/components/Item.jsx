8c50d1cb22823b8b00f4380df005caba042311d2
Gerardo Guerrero <gerardoguerrero29@hotmail.com>
2022-06-08 20:28:53 -0300

    entregable 5

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
 
-export default function Item({id, title, price, description, picture}) {
-  return (
-    <div>Id: {id}  - Producto: {title} - Precio: {price} - Descripcion: {description} - Foto: {picture} </div>
-  )
+export default function Item({ id, title, price, description, picture }) {
+    return (
+
+        <>
+            <div>   - Foto: {picture} </div>
+
+            <Card style={{ width: '15rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                    Descripcion: {description}
+                    <br/>
+                    Precio: {price}
+                    <br/>
+                    Id: {id}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+        </>
+
+    )
 }
