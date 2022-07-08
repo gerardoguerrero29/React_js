e71449c2acd7270698cf8be6c1f9f24081852b69
gerardoguerrero29 <gerardoguerrero29@hotmail.com>
2022-07-07 23:49:56 -0300

    Prueba de git

--- src/components/TestEventos.jsx
+++ src/components/TestEventos.jsx
@@ -10,6 +10,9 @@ export default function TestEventos() {
   return (
     <div onClick={(e)=> {handleClick(e,1)}} style={{ border: "1px solid red" }}>
       TestEventos
+
+holaaaaaaa
+
     </div>
   );
 }
